514db43c506b84bc427878b5c8924626c1da5d87
MayaBenHarush <maya.benharush@post.runi.ac.il>
2024-01-02 16:09:04 +0200

    new time

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,15 +7,12 @@ public class DamkaBoard {
 		int n = Integer.parseInt(args[0]);
 
 		for(int i = 1; i <= n; i++){
-			if(i % 2 == 0)
-				System.out.print( " " );
-
-			for(int c = 1; c <= n; c++){
-				if(c != n)
-					System.out.print( "* " );
-				else 
-					System.out.print( "*" );
-				
+			
+			for(int c = 1; ( i % 2 ) != 0 && c <= n; c++){
+					System.out.print( "* " );	
+			}
+			for(int w = 1; ( i % 2 ) != 0 && c <= n; w++){
+					System.out.print( " *" );	
 			}
 			System.out.println();
 		}		
